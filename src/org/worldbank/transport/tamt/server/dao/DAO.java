e6d56b2216ca3255f33d832c728214cd65b989f8
stuartmoffatt <stuartmoffatt@localhost>
2011-02-19 22:31:43 -0500

    - performance improvements - new features for reducing speedbin tables

--- src/org/worldbank/transport/tamt/server/dao/DAO.java
+++ src/org/worldbank/transport/tamt/server/dao/DAO.java
@@ -12,27 +12,65 @@ import org.postgresql.ds.PGConnectionPoolDataSource;
 abstract class DAO {
 
 	private static Logger logger = Logger.getLogger(DAO.class);
+	protected Connection connection = null;
 	
 	protected Connection getConnection()
 	{
-		logger.debug("Fetching a connection from the pool");
-		Connection connection = null;
-		try {
-			PGConnectionPoolDataSource source = (PGConnectionPoolDataSource)new InitialContext().lookup("TAMTDataSource");
-		    connection = source.getConnection();
-		} catch(SQLException e) {
-		    // log error
-			logger.debug("SQLException: " + e.getMessage());
-		} catch(NamingException e) {
-		    // DataSource wasn't found in JNDI
-			logger.debug("JNDI naming exception: " + e.getMessage());
-		} finally {
-		    //if(connection != null) {
-		    //    try {connection.close();}catch(SQLException e) {}
-		    //}
+		//logger.debug("Get a database connection");
+		/*
+		 * Reuse non-null connections. See if this
+		 * improves performance setting up DB access
+		 * in TAMT_insertSpeedDistributionRecord stored procedure
+		 * found in SpeedBinDAO.insertSpeedDistributionRecord
+		 * 
+		 * Not sure if it will cause thread issues from the
+		 * web front end.
+		 */
+		if( connection == null)
+		{
+			//logger.debug("Connection is null, get one from the pool");
+			try {
+				PGConnectionPoolDataSource source = (PGConnectionPoolDataSource)new InitialContext().lookup("TAMTDataSource");
+			    connection = source.getConnection();
+			} catch(SQLException e) {
+			    // log error
+				logger.debug("SQLException: " + e.getMessage());
+			} catch(NamingException e) {
+			    // DataSource wasn't found in JNDI
+				logger.debug("JNDI naming exception: " + e.getMessage());
+			} finally {
+			    //if(connection != null) {
+			    //    try {connection.close();}catch(SQLException e) {}
+			    //}
+			}
+		} else {
+			
+			try {
+				if( connection.isClosed())
+				{
+					try {
+						PGConnectionPoolDataSource source = (PGConnectionPoolDataSource)new InitialContext().lookup("TAMTDataSource");
+					    connection = source.getConnection();
+					} catch(SQLException e) {
+					    // log error
+						logger.debug("SQLException: " + e.getMessage());
+					} catch(NamingException e) {
+					    // DataSource wasn't found in JNDI
+						logger.debug("JNDI naming exception: " + e.getMessage());
+					} finally {
+					    //if(connection != null) {
+					    //    try {connection.close();}catch(SQLException e) {}
+					    //}
+					}				
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
-		logger.debug("Fetched connection=" + connection.toString());
+		
+		//logger.debug("Fetched connection=" + connection.toString());
 		return connection;
-		   
 	}
 }
